d6ee784ff4e1a63f590b1e26838da47fbd04035a
murod <murad.djanikulov@gmail.com>
2024-03-09 03:37:50 +0500

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for(int i=1; i<=cathetusLength; i++){
+            int x=i;
+            for (int j=i; j<cathetusLength; j++){
+                System.out.print(" ");}
+            for (int j=1; j<=i; j++){
+                System.out.print(x--);}
+            for (int j=2; j<=i; j++){
+                System.out.print(j);}
+            System.out.println();}
     }
 
     public static void main(String[] args) {
